81c39ca342ca6fe5445f0da06975d6a6c7301884
Manuel Laug <manuel.laug@dm.de>
2024-05-17 19:00:47 +0200

    fixes error during settings rendering because method is not found

--- app/models/concerns/foreman_rescue/provisioning_template_extensions.rb
+++ app/models/concerns/foreman_rescue/provisioning_template_extensions.rb
@@ -2,9 +2,17 @@
 
 module ForemanRescue
   module ProvisioningTemplateExtensions
-    def self.templates_by_kind(kind)
-      template_kind = TemplateKind.find_by(name: kind)
-      ProvisioningTemplate.where(:template_kind => template_kind).pluck(:name, :name).to_h
+    def self.prepended(base)
+      class << base
+        prepend ClassMethods
+      end
+    end
+
+    module ClassMethods
+      def templates_by_kind(kind)
+        template_kind = TemplateKind.find_by(name: kind)
+        ProvisioningTemplate.where(:template_kind => template_kind).pluck(:name, :name).to_h
+      end
     end
   end
 end
